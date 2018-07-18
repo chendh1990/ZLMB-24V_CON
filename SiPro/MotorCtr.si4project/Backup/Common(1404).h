fd7e65a65a46e528f57ce68c824b217768922bec
chendh1990 <cuz_1990@sina.com>
2018-07-18 18:34:43 +0800

    忽略不必要的文件

--- SiPro/MotorCtr.si4project/Backup/Common(1404).h
+++
@@ -1,8 +0,0 @@
-#ifndef __COMMON_H__
-#define __COMMON_H__
-#include "Typedef.h"
-#include "N76E003.h"
-#define ENTER_CRITICAL()	EA = 0
-#define EXIT_CRITICAL()		EA = 1
-
-#endif
--- SiPro/MotorCtr.si4project/Backup/Key(6564).c
+++
@@ -1,2 +0,0 @@
-#ifndef __KEY_C__#define __KEY_C__#endif
-#include "N76E003.h"#include "Function_define.h"#include "Typedef.h"#include "Key.h"int a = 10;
